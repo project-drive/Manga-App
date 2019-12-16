75b67efef6e4b6dd18617700d2432d8c522b0925
bhogalprabjot <bhogalprabjot1@gmail.com>
2019-12-16 18:43:56 +0530

    API Test 1

---
+++ manga_api_test/lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Post> fetchPost() async {
+  final response = await http.get('https://www.mangaeden.com/api/list/0/');
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON.
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
+
+class Post {
+  final String id;
+  final String title;
+  final String image;
+
+  Post({this.id, this.title, this.image});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['manga'][50]['i'],
+      title: json['manga'][50]['t'],
+      image: json['manga'][50]['im'],
+    );
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<Post> post;
+
+  @override
+  void initState() {
+    super.initState();
+    post = fetchPost();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const String imglink = 'http://cdn.mangaeden.com/mangasimg/';
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Post>(
+            future: post,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        snapshot.data.title,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Image.network(
+                            imglink+snapshot.data.image,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
